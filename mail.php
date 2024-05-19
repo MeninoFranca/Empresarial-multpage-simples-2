794bef45becc3405de076383ecff1bf3f4ca70de
MeninoFranca <baueremil9@gmail.com>
2024-05-18 23:34:43 -0300

    criando o conteúdo do email que será enviado

--- mail.php
+++ mail.php
@@ -11,7 +11,7 @@ use PHPMailer\PHPMailer\Exception;
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // trata as entradas do formulário.
     $name = strip_tags(trim($_POST["name"]));
-    $name = str_replace(array("\r","\n"),array(" "," "),$name);
+    $name = str_replace(array("\r", "\n"), array(" ", " "), $name);
     $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
     $num = trim($_POST["phone"]);
     $comment = trim($_POST["comment"]);
@@ -21,6 +21,14 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         echo "Por favor, preencha o formulário e tente novamente.";
         exit;
     }
+    $recipient = "comercial.vrpsul@gmail.com";
+
+    $sub = "Contato pelo formulário do site: $name";
+
+    $email_content = "Nome: $name\n";
+    $email_content .= "E-mail: $email\n\n";
+    $email_content .= "Telefone: $num\n\n";
+    $email_content .= "Mensagem:\n$comment\n";
 }
 
 ?>
