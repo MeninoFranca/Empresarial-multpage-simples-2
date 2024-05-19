b9763a43ea518666419b0d40eb2379d0327b24d9
MeninoFranca <baueremil9@gmail.com>
2024-05-18 23:37:59 -0300

    Enviar email atráves do PHPmailer

--- mail.php
+++ mail.php
@@ -29,6 +29,40 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email_content .= "E-mail: $email\n\n";
     $email_content .= "Telefone: $num\n\n";
     $email_content .= "Mensagem:\n$comment\n";
+
+    $mail = new PHPMailer();
+
+    try {
+        // Configure PHPMailer.
+        $mail->isSMTP();
+        $mail->Host = 'smtp.exemplo(pegar no e-mail).com';
+        $mail->SMTPAuth = true;
+        $mail->Username = 'comercial.vrpsul@gmail.com';
+        $mail->Password = 'senhaaqui';
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+        $mail->Port = 587;
+
+        $mail->setFrom($email, $name);
+        $mail->addAddress($recipient);
+        $mail->Subject = $sub;
+        $mail->Body = $email_content;
+
+        if ($mail->send()) {
+            http_response_code(200);
+            echo "Obrigado! Seu comentário foi registrado.";
+        } else {
+            http_response_code(500);
+            echo "Oops! Algo deu errado e nós não conseguimos enviar sua mensagem.";
+        }
+    } catch (Exception $e) {
+        http_response_code(500);
+        echo "Oops! Algo deu errado e nós não conseguimos enviar sua mensagem.";
+    }
+
+} else {
+    http_response_code(403);
+    echo "Houve algum problema com sua requisição, tente novamente.";
+
 }
 
 ?>
